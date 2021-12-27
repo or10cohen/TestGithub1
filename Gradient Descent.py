be129a0fcbd0e03916eb4f4d1c98c2f39a49b56c
or_cohen <or20cohen@gmail.com>
2021-12-27 14:38:44 +0200

    1212121221

---
+++ Gradient Descent.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.array([0, 1, 2])
+y = np.array([1, 3, 7])
+
+iter = 200
+learningRate = [0.01, 0.1, 1]
+
+def GradientDecsent(x, y, learningRate = 1, iter = 50):
+    m = 0
+    b = 0
+    for _ in range(iter):
+        for i in range(len(x)):
+            xi = x[i]
+            yi = y[i]
+            guess = m * xi + b
+            error = yi - guess
+            m = m + error * xi * learningRate
+            b = b + error * learningRate
+    return m, b
+
+
+GradientDecsent(x, y)
+# print(m)
+# print(b)
+# guss = y1
+#
+# for i in range(len(x))
+#     CostFunction = (guss[i] - y[i]) ** 2
+#     CostFunction += CostFunction
