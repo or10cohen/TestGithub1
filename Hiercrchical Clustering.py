e5bab2875b9f4af6cd5e1e79b2b2e722c09a05ac
or_cohen <or20cohen@gmail.com>
2022-02-28 10:48:49 +0200

    new update

--- Hiercrchical Clustering.py
+++ Hiercrchical Clustering.py
@@ -1,7 +1,4 @@
-import colorama
-import matplotlib.pyplot as plt
-from colorama import Fore, Back, Style
-colorama.init(autoreset = True)
+import matplotlib.pyplot
 import numpy as np
 import scipy
 import sklearn
@@ -9,11 +6,13 @@ from matplotlib import pyplot as plt
 from sklearn import datasets
 from scipy.spatial import distance_matrix
 import seaborn as sns
+import scipy.cluster.hierarchy as sch
+
 # from skbio.stats.distance import DissimilarityMatrix
+
 
 iris = datasets.load_iris()
 X = iris.data[:, :]
-print(Fore.LIGHTGREEN_EX + '\nshape train data:', X.shape)
 
 
 class HierarchicalClustering:
@@ -22,6 +21,7 @@ class HierarchicalClustering:
         self.train_data = Data
         self.Normalize_training_data = self.Normalize_data()
         self.distance_matrix = self.Calc_distance_matrix()
+        self.lst = [[] for i in range(3)]
 
     def Normalize_data(self):
         self.Normalize_training_data = sklearn.preprocessing.normalize(self.train_data, norm='l2', axis=0, copy=True)
@@ -29,17 +29,27 @@ class HierarchicalClustering:
 
     def Calc_distance_matrix(self):
         self.distance_matrix = scipy.spatial.distance_matrix(self.Normalize_training_data \
-                                          , self.Normalize_training_data, p=2)
+                                                             , self.Normalize_training_data, p=2)
+
         return self.distance_matrix
 
-    # def
+    def UpdateDistanceMatrix(self, index1, index2):
+        self.distance_matrix = np.delete(self.distance_matrix, (index1[0], index2[0]), axis=0)
+        first_column = self.distance_matrix[:, index1[1]]
+        second_column = self.distance_matrix[:, index2[1]]
+        max_column = np.maximum(first_column, second_column)
+        self.distance_matrix = np.delete(self.distance_matrix, (index1[1], index2[1]), axis=1)
+
+
+
+
+
+
+
 
 
 HC = HierarchicalClustering(X)
-
-print(Fore.LIGHTBLUE_EX + '\nNormalize training data sample:\n', HC.Normalize_training_data[:5, :])
-print(Fore.LIGHTBLUE_EX + '\ntype distance Matrix:', type(HC.distance_matrix))
-print(Fore.LIGHTBLUE_EX + '\nshape distance Matrix:', HC.distance_matrix.shape)
-print(Fore.LIGHTBLUE_EX + '\ndistance Matrix sample:\n', HC.distance_matrix[:5, :5])
 # sns.heatmap(HC.distance_matrix[:10, :10], cmap=plt.cm.Reds)
 # plt.show()
+i, j = np.where(HC.distance_matrix == np.min(HC.distance_matrix[np.nonzero(HC.distance_matrix)]))
+print(i[0], j[0])
