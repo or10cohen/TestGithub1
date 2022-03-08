e91703ba24bb953a8d77984b9ecb6bb97d2944c1
or_cohen <or20cohen@gmail.com>
2022-03-08 14:50:08 +0200

    New Gui for Hiercrchical_Clustering

---
+++ Gui.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import Hiercrchical_Clustering
+from sklearn import datasets
+from PIL import Image
+
+
+
+st.title('Hiercrchical Clustering')
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.radio(
+    'Which DataSet you want to use?',
+    ('iris', 'wine', 'brest cancer', 'diabetes'))
+add_cluster = st.slider('How many cluster you want?(Hyperparameter)', 2, 3, 4)
+add_dimensions = st.slider('Plot with how many dimensions?', 2, 3)
+
+
+if add_selectbox == 'iris' :
+    dataset = datasets.load_iris()
+elif add_selectbox == 'diabetes':
+    dataset = datasets.load_diabetes()
+elif add_selectbox == 'wine':
+    dataset = datasets.load_wine()
+elif add_selectbox == 'brest cancer' :
+    dataset = datasets.load_breast_cancer()
+else:
+    pass
+X = dataset.data[:, :]
+
+
+
+HC = Hiercrchical_Clustering.HierarchicalClustering(X,add_cluster)
+HC.fit()
+if add_dimensions == 2 :
+    HC.Print_2d()
+    image = Image.open('C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\Print_2d.png')
+    st.image(image)
+elif add_dimensions == 3:
+    HC.Print_3d()
+    image = Image.open('C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\Print_3d.png')
+    st.image(image)
+else:
+    pass
+
+
+# run_my_function = Hiercrchical_Clustering.HierarchicalClustering(X)
+
+# st run C:/Users/or_cohen/PycharmProjects/TestGithub1/Gui.py [ARGUMENTS]
