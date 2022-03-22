30b3f48e3c3bfce24e3162b010f2fbecc242b8fc
or_cohen <or20cohen@gmail.com>
2022-03-22 17:10:40 +0200

    update

--- SVM Or.py
+++ SVM Or.py
@@ -1,13 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset = True)
+from PIL import Image
 
 
 
@@ -19,7 +21,10 @@ class svm:
         self.X_train, self.X_test, self.y_train, self.y_test = self.split_data()
         self.normalize_X_train, self.normalize_X_test = self.normalize_data()
         self.model = self.fit()
-        self.confusion_matrixes, self.classification_reports = self.predict()
+        self.predict_on_model, self.confusion_matrices, self.confusion_matrix_displays, self.classification_reports \
+                                                                                                     = self.predict()
+        self.plot_confusion_matrices = self.plot_confusion_matrices()
+
 
     def __str__(self):
         print(Fore.RED + '\ndataset.keys()\n', self.data_set.keys())
@@ -45,18 +50,35 @@ class svm:
         return normalize_train_data, normalize_test_data
 
     def fit(self):
-        model = SVC()
+        model = SVC(C=self.C, kernel=self.kernel)
         model.fit(self.normalize_X_train, self.y_train)
         return model
 
     def predict(self):
         predict_on_model = self.model.predict(self.normalize_X_test)
-        confusion_matrixes = confusion_matrix(self.y_test, predict_on_model)
+        confusion_matrices = confusion_matrix(self.y_test, predict_on_model)
+        confusion_matrix_displays = ConfusionMatrixDisplay(confusion_matrix=confusion_matrices, \
+                                    display_labels=self.model.classes_)
         classification_reports = classification_report(self.y_test, predict_on_model)
-        return confusion_matrixes, classification_reports
+        return predict_on_model, confusion_matrices, confusion_matrix_displays, classification_reports
 
+    def plot_confusion_matrices(self):
+        self.confusion_matrix_displays.plot()
+        plt.savefig('SVM_confusion_matrix_displays.png')
+        return None
 
 if __name__ == '__main__':
     are_dataset = datasets.load_breast_cancer()
     run_svm = svm(are_dataset)
-    # run_svm.__str__()
+    print(Fore.RED + '\n', run_svm.__str__())
+    print(Fore.RED + '\nrun_svm.predict_on_model\n', run_svm.predict_on_model)
+    print(run_svm.predict_on_model.shape)
+    print(Fore.RED + '\nrun_svm.y_test\n', run_svm.y_test)
+    print(run_svm.y_test.shape)
+    print(Fore.RED + '\nconfusion_matrixes\n', run_svm.confusion_matrices)
+    # z = run_svm.confusion_matrices
+    # print('\nTotal label 0: {} \nTrue: {}  False: {} \n\nTotal label 1: {} \nTrue: {}  False: {}' .format(z[0][0] + z[0][1], \
+    # z[0][0], z[0][1], z[1][0] + z[1][1], z[1][1], z[1][0]))
+    print(Fore.RED + '\nclassification_reports\n', run_svm.classification_reports)
+    image = Image.open("SVM_confusion_matrix_displays.png")
+    Image._show(image)
