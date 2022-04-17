4379410a0378ad00957d31dbf03e16652eb870ff
or_cohen <or20cohen@gmail.com>
2022-04-17 15:24:26 +0300

    min_distance

--- K-means.py
+++ K-means.py
@@ -1,11 +1,10 @@
 import numpy as np
-import numpy.random
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import datasets
 
 
-class K_means:
+class KMeans:
     def __init__(self, Data, number_clusters=2, max_distance=200, test_size=0.01, random_state=42, dimension=2):
         self.Data = Data
         self.number_clusters, self.max_distance, self.test_size, self.random_state, self.len_data, self.dimension \
@@ -13,8 +12,12 @@ class K_means:
         self.X, self.y = self.data()
         self.X_train,self.X_test, self.y_train, self.y_test = self.split_data()
         self.normalize_data, self.normalize_test_data = self.normalize_data()
+        self.random_points_from_data = self.start_points()
+        self.distance_vector = self.points_distance()
+        self.min_distance = min_distance()
 
     def data(self):
+        # add Error if input self.dimension > No. of features
         X = self.Data.data[:, :self.dimension]
         y = self.Data.target
         return X, y
@@ -30,14 +33,33 @@ class K_means:
         return normalize_train_data, normalize_test_data
 
     def start_points(self):
-        X_train_array = np.array(self.X_train)
-        numpy.random.choice(X_train_array, size=self.number_clusters, replace=False)
+        # add more options at start points
+        random_points_from_data = np.random.randint(len(self.X_train), size=self.number_clusters)
+        random_points_from_data = self.X_train[random_points_from_data]
+        return random_points_from_data
+
+    def points_distance(self):
+        distance_vector = [[i] for i in range(self.number_clusters)]
+        k = -1
+        for i in self.random_points_from_data:
+            k += 1
+            for j in self.X_train:
+                distance = np.linalg.norm(i - j)
+                distance_vector[k].append(distance)
+        return distance_vector
+
+    def min_distance(self):
+        print(self.distance_vector[0])
+        return min_distance
+
 
 if __name__ == '__main__':
     dataset = datasets.load_iris()
-    run_K_means = K_means(dataset)
+    run_K_means = KMeans(dataset)
     print("Or Yosef Cohen")
-    print(len(run_K_means.Data.data))
-    print(len(run_K_means.X_train))
-    print(len(run_K_means.X_test))
-    print(run_K_means.X_train)
+    # print(len(run_K_means.Data.data))
+    # print(len(run_K_means.X_train))
+    # print(len(run_K_means.X_test))
+    # print(run_K_means.X_train)
+    # print(run_K_means.random_points_from_data)
+    # print(run_K_means.distance_vector)
