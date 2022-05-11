45953758d858e779baa55c59316d4c19afa1dd83
or_cohen <or20cohen@gmail.com>
2022-05-11 16:55:02 +0300

    min_distance

--- K-means.py
+++ K-means.py
@@ -4,24 +4,24 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn import datasets
 import random
 from scipy import spatial
+import matplotlib.pyplot as plt
+
 
 class KMeans:
-    def __init__(self, Data, number_clusters=3, max_distance=200, test_size=0.01, random_state=42, dimension=3):
+    def __init__(self, Data, number_of_clusters=3, test_size=0.01, random_state=42, number_of_features=3):
         self.Data = Data
-        self.number_clusters, self.max_distance, self.test_size, self.random_state, self.len_data, self.dimension \
-            = number_clusters, max_distance, test_size, random_state, len(Data), dimension
+        self.number_clusters, self.test_size, self.random_state, self.len_data, self.number_of_features \
+            = number_of_clusters, test_size, random_state, len(Data), number_of_features
         self.X, self.y = self.data()
-        self.X_train,self.X_test, self.y_train, self.y_test = self.split_data()
+        self.X_train, self.X_test, self.y_train, self.y_test = self.split_data()
         self.normalize_data, self.normalize_test_data = self.normalize_data()
-        self.random_points_from_data = self.start_points()
-        self.distance_matrix = self.distance_matrix()
-        self.who_more_close_at_distance_matrix_per_column = self.who_more_close()
-        self.cluster = self.make_clusters()
-        self.mean_values = self.finding_new_centroid()
+        self.distance_matrix = None
+        self.closestpoints = None
+        self.cluster = []
 
     def data(self):
         # add Error if input self.dimension > No. of features
-        X = self.Data.data[:, :self.dimension]
+        X = self.Data.data[:, :self.number_of_features]
         y = self.Data.target
         return X, y
 
@@ -39,74 +39,65 @@ class KMeans:
         # add more options at start points
         # random_index_from_data1 = np.random.randint(len(self.X_train), size=self.number_clusters) #duplicates start point!
         random_index_from_data = random.sample([i for i in range(len(self.X_train))], self.number_clusters)
-        random_points_from_data = self.X_train[random_index_from_data]
+        initial_random_points_from_data = self.X_train[random_index_from_data]
+
+        random_points = np.array([np.random.rand(self.number_of_features)] for i in range(self.number_clusters))
         # X_train_without_start_points = np.delete(self.X_train, random_index_from_data, 0)
         # return X_train_without_start_points, random_points_from_data
-        return random_points_from_data
+        return initial_random_points_from_data, random_points
 
-    def distance_matrix(self, points):
-        distance_matrix = spatial.distance_matrix(points, self.X_train, p=2)
-        return distance_matrix
+    def Calc_distance_matrix(self, points):
+        self.distance_matrix = spatial.distance_matrix(points, self.X_train, p=2)
 
-    def who_more_close(self):
-        who_more_close_at_distance_matrix_per_column = np.argmin(self.distance_matrix, axis=0)
-        return who_more_close_at_distance_matrix_per_column
+    def closest_points(self):
+        self.closestpoints = np.argmin(self.distance_matrix, axis=0)
 
     def make_clusters(self):
-        clusters = []
         for i in range(self.number_clusters):
-            clusters.append(self.X_train[np.where(self.who_more_close_at_distance_matrix_per_column == i)])
-        return clusters
+            self.cluster.append(self.X_train[np.where(self.closestpoints == i)])
 
     def finding_new_centroid(self):
-        mean_values = []
+        new_centroid = []
         for i in range(self.number_clusters):
-            mean_values.append(self.cluster[i].mean(axis=0))
-        return mean_values
+            new_centroid.append(self.cluster[i].mean(axis=0))
+        return new_centroid
 
     def run(self, number_of_iterations):
-        points = self.random_points_from_data()
+        points, points2 = self.start_points()
+        print(type(points))
+        print(type(points2))
+        print(len(points))
+        print(len(points2))
+
         i = 0
         while i < number_of_iterations:
-            self.distance_matrix(points)
-            self.who_more_close()
+            self.Calc_distance_matrix(points)
+            self.closest_points()
             self.make_clusters()
-            points = self.finding_new_centroid()
+            if i < number_of_iterations - 1:
+                points = self.finding_new_centroid()
+                self.cluster.clear()
             i += 1
+        return points
+
+    def WSS(self, central_points):
+        result = []
+        for i in range(0, len(central_points)):
+            central_points[i] = np.reshape(central_points[i], (-1, len(central_points[i])))
+            result.append(spatial.distance_matrix(central_points[i], self.cluster[i], p=2).sum())
+        result = np.array(result).sum()
+        return result
 
 
 if __name__ == '__main__':
     dataset = datasets.load_iris()
-    print(dataset.keys())
-    run_K_means = KMeans(dataset)
-    print(type(run_K_means.X_train_without_start_points))
-    # print(run_K_means.distance_matrix)
-    print(run_K_means.mean_values)
-
-
-    # print("Or Yosef Cohen")
-    # # print(len(run_K_means.Data.data))
-    # # print(len(run_K_means.X_train))
-    # # print(len(run_K_means.X_test))
-    # # print(len(run_K_means.X_train))
-    # # print(len(run_K_means.X_train_without_start_points))
-    # # print(len(run_K_means.random_points_from_data))
-    # print(run_K_means.random_points_from_data)
-    #
-    # print(run_K_means.distance_vector[0])
-    # print(run_K_means.distance_vector[1])
-    # print(run_K_means.min_distance_index)
-    # print(run_K_means.distance_vector[0][run_K_means.min_distance_index[0]])
-    # print(run_K_means.distance_vector[1][run_K_means.min_distance_index[1]])
-    # # print(run_K_means.distance_vector[0].sort())
-    # print(run_K_means.distance_vector[0])
-    # # print(run_K_means.distance_vector[1].sort())
-    # print(run_K_means.distance_vector[1])
-    #
-    # # print(len(run_K_means.distance_vector))
-    # # print(len(run_K_means.distance_vector[0]))
-    # # print(len(run_K_means.distance_vector[1]))
-    # # print(type(run_K_means.distance_vector[0]))
-    # # print(min(run_K_means.distance_vector[0]))
-    # # print(min(run_K_means.distance_vector[1]))
-    # # print(run_K_means.min_distance)
+    # run_K_means = KMeans(dataset)
+    number_clusters = []
+    elbow = []
+    for i in range(1, 8):
+        number_clusters.append(i)
+        run_K_means = KMeans(dataset, number_of_clusters=i)
+        points = run_K_means.run(3)
+        elbow.append(run_K_means.WSS(points))
+    plt.plot(number_clusters, elbow)
+    plt.show()
