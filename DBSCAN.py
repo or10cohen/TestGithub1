0faa36a66bf2c027ba85cb2eaefb66f1051cacc6
or20cohen <or20cohen@gmail.com>
2022-05-23 23:31:11 +0300

    update with Oren

--- DBSCAN.py
+++ DBSCAN.py
@@ -40,25 +40,25 @@ class DBSCAN:
         # return choose_random_index
 
     def neighboursInEpsilon(self):
-        self.distance_matrix_true_false = self.distance_matrix <= self.epsilon
-        self.distance_matrix_where = np.where(0 < self.distance_matrix <= self.epsilon)
-        count_neighbours_in_epsilon = np.count_nonzero(self.distance_matrix, axis=1)
-        return count_neighbours_in_epsilon
+        self.distance_matrix_true_false = self.distance_matrix <= self.epsilon #
+        # self.distance_matrix_where = np.where(0 < self.distance_matrix <= self.epsilon) #
+        count_neighbours_in_epsilon = np.count_nonzero(self.distance_matrix_true_false, axis=1) #
+        return count_neighbours_in_epsilon #
 
     def corePoint(self):
-        self.core_point_true_false = self.count_neighbours_in_epsilon > self.minPts
+        self.core_point_true_false = self.count_neighbours_in_epsilon >= self.minPts
 
-    def chooseCorePoint(self, index):
+    def chooseCorePoint(self):
         run = True
         while run:
             self.randomPoint()
-            if self.core_point_true_false[index]:
+            if self.core_point_true_false[self.choose_random_index]:
                 run = False
 
-    def reachablePoints(self, index):
-        reachable_points = self.distance_matrix_true_false[index]
-        reachable_points = np.where(reachable_points)
-        return reachable_points
+    def directReachablePoints(self, index):
+        direct_reachable_points = self.distance_matrix_true_false[index]
+        direct_reachable_points = np.where(direct_reachable_points)
+        return direct_reachable_points
 
     def makeClusters(self, index, cluster):
         for ins in index:
@@ -68,25 +68,27 @@ class DBSCAN:
         for ins in index:
             self.list_of_index.pop(ins)
 
-    def chooseClosePoint(self, index):
-        copy_distance_matrix = self.distance_matrix
-        copy_distance_matrix += np.diag([np.inf] * len(self.distance_matrix))
-        index_close_point = copy_distance_matrix[index].argmin()
-        return index_close_point
-
-
     def run(self):
-        #1.choose core point first
-        self.chooseCorePoint(self.list_of_index)
-        #2.cluster core point + reachable points and pop all
         cluster = 0
+        #pop all noise points
+        noise_index = np.where(self.count_neighbours_in_epsilon == 0)
+        self.popFromIndex(noise_index)
+        #cluster all points without noise
         while len(self.list_of_index) > 0:
-            index_reachable_points = self.reachablePoints(self.choose_random_index)
+            self.chooseCorePoint()
+            index_reachable_points = self.directReachablePoints(self.choose_random_index)
             self.makeClusters(index_reachable_points, cluster)
-            self.popFromIndex(index_reachable_points)
-            index_close_point = self.chooseClosePoint(index_reachable_points)
-            index_close_point
 
+            index_reachable_points.pop()  #pop corePts
 
-
-
+            while index_reachable_points > 0:   # add self.list_of_index.pop
+                if self.core_point_true_false[index_reachable_points[0]]:
+                    index_reachable_points2 = self.directReachablePoints(index_reachable_points[0])
+                    for ind in index_reachable_points2:
+                        if self.cluster[ind] is not None:
+                            index_reachable_points2.pop(ind)      #maybe need list
+                            self.list_of_index.pop(ind)
+                    index_reachable_points.extend(index_reachable_points2)
+                index_reachable_points.pop(0)
+                self.makeClusters(index_reachable_points, cluster)
+            cluster += 1
