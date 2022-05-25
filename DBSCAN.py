0304e103223bf256344f5dc23a56d6d55894edaf
or_cohen <or20cohen@gmail.com>
2022-05-25 13:41:09 +0300

    new

--- DBSCAN.py
+++ DBSCAN.py
@@ -8,18 +8,17 @@ import matplotlib.pyplot as plt
 
 
 class DBSCAN:
-    def __init__(self, data, number_of_features=3, epsilon=1, minPts=3):
+    def __init__(self, data, number_of_features=3, epsilon=0.3, minPts=3):
         self.Data, self.epsilon, self.minPts, self.len_data, self.number_of_features \
-            = data, epsilon, minPts, len(data), number_of_features
+            = data, epsilon, minPts, len(data.data), number_of_features
         self.X = self.data()
         self.normalize_data = self.normalizeData()
         self.distance_matrix = self.calcDistancematrix()
-        self.count_neighbours_in_epsilon = self.neighboursInEpsilon()
+        self.count_neighbours_in_epsilon, self.distance_matrix_true_false = self.neighboursInEpsilon()
         self.list_of_index = list(range(self.len_data))
         self.choose_random_index = None
-        self.distance_matrix_true_false = None
         self.distance_matrix_where = None
-        self.core_point_true_false = None
+        self.core_point_true_false = self.corePoint()
         self.cluster = [None] * self.len_data
 
     def data(self):
@@ -40,13 +39,14 @@ class DBSCAN:
         # return choose_random_index
 
     def neighboursInEpsilon(self):
-        self.distance_matrix_true_false = self.distance_matrix <= self.epsilon #
-        # self.distance_matrix_where = np.where(0 < self.distance_matrix <= self.epsilon) #
-        count_neighbours_in_epsilon = np.count_nonzero(self.distance_matrix_true_false, axis=1) #
-        return count_neighbours_in_epsilon #
+        distance_matrix_true_false = self.distance_matrix <= self.epsilon
+        # self.distance_matrix_where = np.where(0 < self.distance_matrix <= self.epsilon)
+        count_neighbours_in_epsilon = np.count_nonzero(distance_matrix_true_false, axis=1)
+        return count_neighbours_in_epsilon, distance_matrix_true_false
 
     def corePoint(self):
-        self.core_point_true_false = self.count_neighbours_in_epsilon >= self.minPts
+        core_point_true_false = self.count_neighbours_in_epsilon >= self.minPts
+        return core_point_true_false
 
     def chooseCorePoint(self):
         run = True
@@ -68,44 +68,57 @@ class DBSCAN:
         for ins in index:
             self.list_of_index.remove(ins)
 
+
+
     def run(self):
         cluster = 0
         #pop all noise points
-        noise_index = np.where(self.count_neighbours_in_epsilon == 0)
+        noise_index = np.concatenate(np.where(self.count_neighbours_in_epsilon == 1)).tolist() #where and convert to list
         self.popFromIndex(noise_index)
         #cluster all points without noise
+
         while len(self.list_of_index) > 0:
             self.chooseCorePoint()
             index_reachable_points = self.directReachablePoints(self.choose_random_index)
-            self.makeClusters(index_reachable_points, cluster)
+            index_reachable_points_list = np.concatenate(index_reachable_points).tolist() #convert to list
 
-            self.popFromIndex(index_reachable_points)
+            self.makeClusters(index_reachable_points_list, cluster)
+            self.popFromIndex(index_reachable_points_list)
+            index_reachable_points_list.remove(self.choose_random_index) #pop corePts
 
-            index_reachable_points = np.delete(index_reachable_points, np.where(index_reachable_points == self.choose_random_index)) #pop corePts
-            # index_reachable_points.remove(self.choose_random_index)  #pop corePts
 
-            while len(index_reachable_points) > 0:   # add self.list_of_index.pop
-                if self.core_point_true_false[index_reachable_points[0]]:
-                    index_reachable_points2 = self.directReachablePoints(index_reachable_points[0])
-                    for ind in index_reachable_points2:
+            while len(index_reachable_points_list) > 0:   # add self.list_of_index.pop
+                if self.core_point_true_false[index_reachable_points_list[0]]:
+                    index_reachable_points2 = self.directReachablePoints(index_reachable_points_list[0])
+                    index_reachable_points2_list = np.concatenate(index_reachable_points2).tolist() #convert to list\
+                    index_reachable_points2_list.pop(0)
+                    for ind in index_reachable_points2_list:
                         if self.cluster[ind] is not None:
-                            # index_reachable_points2.pop(ind)      #maybe need list
-                            index_reachable_points2 = np.delete(index_reachable_points2, np.where(index_reachable_points == idx))
-                        self.popFromIndex(ind)
-                    index_reachable_points = np.append(index_reachable_points, index_reachable_points2)
+                            index_reachable_points2_list.remove(ind)
+                        elif ind in self.list_of_index:
+                            self.list_of_index.remove(ind)
+                        else:
+                            pass
+                    index_reachable_points_list.extend(index_reachable_points2_list)
                     # index_reachable_points.extend(index_reachable_points2)
-                index_reachable_points = np.delete(index_reachable_points, 0)
+                else:
+                    self.makeClusters(index_reachable_points_list[0], cluster)
+                    index_reachable_points_list.remove(index_reachable_points_list[0])
+
+
+                # index_reachable_points_list = np.delete(index_reachable_points_list, 0)
                 # index_reachable_points.pop(0)
-                self.makeClusters(index_reachable_points, cluster)
+                self.makeClusters(index_reachable_points_list, cluster)
             cluster += 1
 
 
 if __name__ == '__main__':
     dataset = datasets.load_iris()
     run_DBSCAN = DBSCAN(data=dataset)
-    print(run_DBSCAN.len_data)
+    run_DBSCAN.run()
+    # print(run_DBSCAN.len_data)
+    # print(run_DBSCAN.distance_matrix)
+    # print(run_DBSCAN.distance_matrix_true_false)
+    # print(run_DBSCAN.count_neighbours_in_epsilon)
+    # print(run_DBSCAN.core_point_true_false)
     print(run_DBSCAN.cluster)
-    print(run_DBSCAN.distance_matrix)
-    print(run_DBSCAN.distance_matrix_true_false)
-    print(run_DBSCAN.count_neighbours_in_epsilon)
-    print(run_DBSCAN.core_point_true_false)
