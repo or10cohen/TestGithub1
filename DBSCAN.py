2021eada1ba8497efb7970938e011691b187eaf2
or20cohen <or20cohen@gmail.com>
2022-05-30 23:45:59 +0300

    update with Oren&Mordechai

--- DBSCAN.py
+++ DBSCAN.py
@@ -67,58 +67,67 @@ class DBSCAN:
     def popFromIndex(self, index):
         for ins in index:
             self.list_of_index.remove(ins)
+            # for i in range(self.len_data):
+            #     self.distance_matrix_true_false[ins][i] = False
 
 
 
     def run(self):
         cluster = 0
         #pop all noise points
+        print('list of index', self.list_of_index)
+        print('len list of index', len(self.list_of_index))
         noise_index = np.concatenate(np.where(self.count_neighbours_in_epsilon == 1)).tolist() #where and convert to list
         self.popFromIndex(noise_index)
+        print("noise index", noise_index)
+        print("len noise index", len(noise_index))
+        print('list of index after remove noise', self.list_of_index)
+        print('len list of index after remove noise', len(self.list_of_index))
         #cluster all points without noise
-
+        print("\n\n\n-------------------------------------------------------------\n\n\n")
         while len(self.list_of_index) > 0:
             self.chooseCorePoint()
+            print("core point", self.choose_random_index)
             index_reachable_points = self.directReachablePoints(self.choose_random_index)
             index_reachable_points_list = np.concatenate(index_reachable_points).tolist() #convert to list
+            print('index_reachable_points_list', index_reachable_points_list)
+            print('len index_reachable_points_list', len(index_reachable_points_list))
+            print("\n\n\n-------------------------------------------------------------\n\n\n")
+            # while len(index_reachable_points_list) > 0:   # add self.list_of_index.pop
+            for point in index_reachable_points_list:
+                print("index_reachable_points_list", index_reachable_points_list)
+                print("point", point)
+                if self.core_point_true_false[point]:
+                    print("T\F", self.core_point_true_false[point])
+                    index_reachable_points2 = self.directReachablePoints(point)
+                    index_reachable_points2_list = np.concatenate(index_reachable_points2).tolist() #convert to list\
+                    print('index_reachable_points2_list', index_reachable_points2_list)
+                    for point2 in index_reachable_points2_list:
+                        print("i", point2)
+                        if point2 in index_reachable_points_list:
+                            print("not need you!")
+                        else:
+                            print("I need you!")
+                            index_reachable_points_list.append(point2)
+                else:
+                    print("T\F", self.core_point_true_false[point])
 
             self.makeClusters(index_reachable_points_list, cluster)
             self.popFromIndex(index_reachable_points_list)
-            index_reachable_points_list.remove(self.choose_random_index) #pop corePts
-
-
-            while len(index_reachable_points_list) > 0:   # add self.list_of_index.pop
-                if self.core_point_true_false[index_reachable_points_list[0]]:
-                    index_reachable_points2 = self.directReachablePoints(index_reachable_points_list[0])
-                    index_reachable_points2_list = np.concatenate(index_reachable_points2).tolist() #convert to list\
-                    index_reachable_points2_list.pop(0)
-                    for ind in index_reachable_points2_list:
-                        if self.cluster[ind] is not None:
-                            index_reachable_points2_list.remove(ind)
-                        elif ind in self.list_of_index:
-                            self.list_of_index.remove(ind)
-                        else:
-                            pass
-                    index_reachable_points_list.extend(index_reachable_points2_list)
-                    # index_reachable_points.extend(index_reachable_points2)
-                else:
-                    self.makeClusters(index_reachable_points_list[0], cluster)
-                    index_reachable_points_list.remove(index_reachable_points_list[0])
-
-
-                # index_reachable_points_list = np.delete(index_reachable_points_list, 0)
-                # index_reachable_points.pop(0)
-                self.makeClusters(index_reachable_points_list, cluster)
+            index_reachable_points = []
             cluster += 1
-
+            print("cluster += 1, cluster:", cluster)
+            print("\n\n\n-------------------------------------------------------------\n\n\n")
+            #---------------------more outliers??need to check----------------------------------------------
+            if len(self.list_of_index) == 8:
+                noise_index2 = self.list_of_index
+                print("what left after all clusters? noise again? need to check!", noise_index2)
+                self.list_of_index.clear()
+                print("self.list_of_index", self.list_of_index)
+                print(len(self.list_of_index))
 
 if __name__ == '__main__':
     dataset = datasets.load_iris()
     run_DBSCAN = DBSCAN(data=dataset)
     run_DBSCAN.run()
-    # print(run_DBSCAN.len_data)
-    # print(run_DBSCAN.distance_matrix)
-    # print(run_DBSCAN.distance_matrix_true_false)
-    # print(run_DBSCAN.count_neighbours_in_epsilon)
-    # print(run_DBSCAN.core_point_true_false)
     print(run_DBSCAN.cluster)
