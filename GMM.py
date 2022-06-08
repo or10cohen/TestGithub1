0d92dc1565d63533822ba37b60bdb41685506cfa
or_cohen <or20cohen@gmail.com>
2022-06-08 12:54:08 +0300

    new

--- GMM.py
+++ GMM.py
@@ -3,11 +3,15 @@ import scipy as sc
 from sklearn import datasets
 from sklearn.cluster import KMeans
 from PIL import Image
-
+import future
+import tk
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 class GMM:
-    def __init__(self, data, number_of_clusters):
-        self.X, self.n_clusters =  data, number_of_clusters
+    def __init__(self, data, number_of_clusters=3, n_epochs=30):
+        self.X, self.n_clusters, self.n_epochs =  data, number_of_clusters, n_epochs
         self.totals = None
         self.clusters = []
         self.gamma_nk = []
@@ -31,7 +35,7 @@ class GMM:
             self.clusters.append({
                 'pi_k': 1.0 / self.n_clusters,
                 'mu_k': mu_k[i],
-                'cov_k': cov_matrix  # need to use mu_k----Oren you use **data** for the covariance matrix
+                'cov_k': cov_matrix  # need to use mu_k----Oren you use **data** for the covariance matrix----
             })
         return self.clusters
 
@@ -41,18 +45,18 @@ class GMM:
         N = self.X.shape[0]  # number of rows
         K = len(self.clusters)
         self.totals = np.zeros((N, 1), dtype=np.float64)
-        gamma_nk = np.zeros((N, K), dtype=np.float64)
+        self.gamma_nk = np.zeros((N, K), dtype=np.float64)
 
         for k, cluster in enumerate(self.clusters):
             pi_k = cluster['pi_k']
             mu_k = cluster['mu_k']
             cov_k = cluster['cov_k']
 
-            gamma_nk[:, k] = (pi_k * self.gaussian(self.X, mu_k, cov_k)).ravel()
+            self.gamma_nk[:, k] = (pi_k * self.gaussian(self.X, mu_k, cov_k)).ravel()
 
-        self.totals = np.sum(gamma_nk, 1)
-        gamma_nk /= np.expand_dims(self.totals, 1)
-        return self.gamma_nk
+        self.totals = np.sum(self.gamma_nk, 1)
+        self.gamma_nk /= np.expand_dims(self.totals, 1)
+
 
     def maximization_step(self):
         N = float(self.X.shape[0])
@@ -82,13 +86,19 @@ class GMM:
         sample_likelihoods = np.log(self.totals)
         return np.sum(sample_likelihoods), sample_likelihoods
 
-    def train_gmm(self, n_epochs):
+    def plot_likelihood(self):
+        plt.figure(figsize=(10, 10))
+        plt.title('Log-Likelihood')
+        plt.plot(np.arange(1, self.n_epochs + 1), self.likelihoods)
+        plt.show()
+
+    def train_gmm(self):
         clusters = self.initialize_clusters()
-        likelihoods = np.zeros((n_epochs,))
+        self.likelihoods = np.zeros((self.n_epochs,))
         scores = np.zeros((X.shape[0], self.n_clusters))
         history = []
         stop = 100
-        for i in range(n_epochs):
+        for i in range(self.n_epochs):
             clusters_snapshot = []
 
             # This is just for our later use in the graphs
@@ -104,13 +114,13 @@ class GMM:
             self.maximization_step()
 
             likelihood, sample_likelihoods = self.get_likelihood()
-            likelihoods[i] = likelihood
+            self.likelihoods[i] = likelihood
 
             print('Epoch: ', i + 1, 'Likelihood: ', likelihood)
 
         scores = np.log(self.gamma_nk)
 
-        return clusters, likelihoods, scores, sample_likelihoods, history
+        return clusters, self.likelihoods, scores, sample_likelihoods, history
 
 
 if __name__ == '__main__':
@@ -118,5 +128,6 @@ if __name__ == '__main__':
     X = data.data
     print(X.shape)
     print(np.cov(X.T).shape)
-    gmm = GMM(X, 4)
-    gmm.train_gmm(15)
+    gmm = GMM(X, number_of_clusters=4)
+    gmm.train_gmm()
+    gmm.plot_likelihood()
