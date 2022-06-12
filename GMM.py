2ddfe8f5e061f522c51b2c9efc115b6497b0d99c
or_cohen <or20cohen@gmail.com>
2022-06-12 15:29:40 +0300

    new

--- GMM.py
+++ GMM.py
@@ -1,30 +1,31 @@
 import numpy as np
+import imageio
 import scipy as sc
 from sklearn import datasets
 from sklearn.cluster import KMeans
 from PIL import Image
-import future
-import tk
-import matplotlib
-matplotlib.use('TkAgg')
+import matplotlib.colors as colors
+import matplotlib.animation as ani
+import matplotlib.cm as cmx
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
 
 class GMM:
-    def __init__(self, data, number_of_clusters=3, n_epochs=30):
-        self.X, self.n_clusters, self.n_epochs =  data, number_of_clusters, n_epochs
+    def __init__(self, data, number_of_clusters=3, n_epochs=30, epsilon_likelihood=1):
+        self.X, self.n_clusters, self.n_epochs, self.epsilon_likelihood = \
+            data, number_of_clusters, n_epochs, epsilon_likelihood
         self.totals = None
         self.clusters = []
         self.gamma_nk = []
-
-    @staticmethod
-    def gaussian(X, mu, cov):
+    # @staticmethod
+    def gaussian(self, mu, cov):
         # Gaussian_P_formula = Image.open("C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\Gaussian_P_formula.PNG")
         # Gaussian_P_formula.show()
-        n = X.shape[1]  # number of columns
-        diff = (X - mu).T
+        n = self.X.shape[1]  # number of columns
+        diff = (self.X - mu).T
         return np.diagonal(1 / ((2 * np.pi) ** (n / 2) * np.linalg.det(cov) ** 0.5) * np.exp(
             -0.5 * np.dot(np.dot(diff.T, np.linalg.inv(cov)), diff))).reshape(-1, 1)
-
     def initialize_clusters(self):
         # We use the KMeans centroids to initialise the GMM
         kmeans = KMeans(self.n_clusters).fit(self.X)
@@ -38,7 +39,6 @@ class GMM:
                 'cov_k': cov_matrix  # need to use mu_k----Oren you use **data** for the covariance matrix----
             })
         return self.clusters
-
     def expectation_step(self):  # calculating responsibility matrix
         # Gamma_nk_formula = Image.open("C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\Gamma_nk_formula.PNG")
         # Gamma_nk_formula.show()
@@ -52,12 +52,10 @@ class GMM:
             mu_k = cluster['mu_k']
             cov_k = cluster['cov_k']
 
-            self.gamma_nk[:, k] = (pi_k * self.gaussian(self.X, mu_k, cov_k)).ravel()
+            self.gamma_nk[:, k] = (pi_k * self.gaussian(mu_k, cov_k)).ravel()
 
         self.totals = np.sum(self.gamma_nk, 1)
         self.gamma_nk /= np.expand_dims(self.totals, 1)
-
-
     def maximization_step(self):
         N = float(self.X.shape[0])
 
@@ -79,48 +77,91 @@ class GMM:
             cluster['pi_k'] = pi_k
             cluster['mu_k'] = mu_k
             cluster['cov_k'] = cov_k
-
     def get_likelihood(self):
         # likelihoods_formula = Image.open("C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\likelihoods_formula.PNG")
         # likelihoods_formula.show()
         sample_likelihoods = np.log(self.totals)
         return np.sum(sample_likelihoods), sample_likelihoods
-
     def plot_likelihood(self):
         plt.figure(figsize=(10, 10))
         plt.title('Log-Likelihood')
         plt.plot(np.arange(1, self.n_epochs + 1), self.likelihoods)
         plt.show()
+        plt.savefig("plot_likelihood.png")
+    def create_cluster_animation(self):
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        colorset1 = ['blue', 'red', 'black']
+        images = []
 
+        for j, clusters in enumerate(self.history):
+            idx = 0
+            if j % 3 != 0:
+                continue
+
+            plt.cla()
+
+            for cluster in self.clusters:
+                mu = cluster['mu_k']
+                cov = cluster['cov_k']
+
+                eigenvalues, eigenvectors = np.linalg.eigh(cov)
+                order = eigenvalues.argsort()[::-1]
+                eigenvalues, eigenvectors = eigenvalues[order], eigenvectors[:, order]
+                vx, vy = eigenvectors[:, 0][0], eigenvectors[:, 0][1]
+                theta = np.arctan2(vy, vx)
+
+                color = colors.to_rgba(colorset1[idx])
+
+                for cov_factor in range(1, 4):
+                    ell = Ellipse(xy=mu, width=np.sqrt(eigenvalues[0]) * cov_factor * 2,
+                                  height=np.sqrt(eigenvalues[1]) * cov_factor * 2, angle=np.degrees(theta), linewidth=2)
+                    ell.set_facecolor((color[0], color[1], color[2], 1.0 / (cov_factor * 4.5)))
+                    ax.add_artist(ell)
+
+                ax.scatter(cluster['mu_k'][0], cluster['mu_k'][1], c=colorset1[idx], s=1000, marker='+')
+                idx += 1
+
+            for i in range(X.shape[0]):
+                ax.scatter(X[i, 0], X[i, 1], c=colorset1[np.argmax(self.scores[i])], marker='o')
+
+            fig.canvas.draw()
+
+            image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+            image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+            images.append(image)
+
+        kwargs_write = {'fps': 1.0, 'quantizer': 'nq'}
+        imageio.mimsave('./gmm.gif', images, fps=1)
+        plt.show(Image.open('gmm.gif').convert('RGB'))
     def train_gmm(self):
-        clusters = self.initialize_clusters()
+        self.clusters = self.initialize_clusters()
         self.likelihoods = np.zeros((self.n_epochs,))
         scores = np.zeros((X.shape[0], self.n_clusters))
-        history = []
+        self.history = []
         stop = 100
         for i in range(self.n_epochs):
             clusters_snapshot = []
 
             # This is just for our later use in the graphs
-            for cluster in clusters:
+            for cluster in self.clusters:
                 clusters_snapshot.append({
                     'mu_k': cluster['mu_k'].copy(),
                     'cov_k': cluster['cov_k'].copy()
                 })
 
-            history.append(clusters_snapshot)
+            self.history.append(clusters_snapshot)
 
             self.expectation_step()
             self.maximization_step()
 
-            likelihood, sample_likelihoods = self.get_likelihood()
+            likelihood, self.sample_likelihoods = self.get_likelihood()
             self.likelihoods[i] = likelihood
 
             print('Epoch: ', i + 1, 'Likelihood: ', likelihood)
 
-        scores = np.log(self.gamma_nk)
+        self.scores = np.log(self.gamma_nk)
 
-        return clusters, self.likelihoods, scores, sample_likelihoods, history
 
 
 if __name__ == '__main__':
@@ -128,6 +169,7 @@ if __name__ == '__main__':
     X = data.data
     print(X.shape)
     print(np.cov(X.T).shape)
-    gmm = GMM(X, number_of_clusters=4)
+    gmm = GMM(X, number_of_clusters=3)
     gmm.train_gmm()
     gmm.plot_likelihood()
+    # gmm.create_cluster_animation()
---
+++ Gui_GMM.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import numpy as np
+import GMM
+from sklearn import datasets
+from PIL import Image
+from sklearn.utils import Bunch
+import pandas as pd
+
+def main():
+    st.title('GMM')
+    col1, col2, col3 = st.columns([2,5,2])
+    with col1:
+        add_dataset = st.radio(
+            'Which DataSet do you want to use?',
+            ('make 3 circles 2D',  'iris', 'circle in circle 2D', 'blobs 2D', 's_curve 2D'))
+
+        if add_dataset == 'circle in circle 2D':
+            dataset = datasets.make_circles(n_samples=200, shuffle=True, noise=None, random_state=None, factor=0.4)
+            dataset = Bunch(data=dataset[0])
+        elif add_dataset == 'iris' :
+            dataset = datasets.load_iris()
+        elif add_dataset == 'make 3 circles 2D' :
+            X_small, y_small = datasets.make_circles(n_samples=(200, 200), random_state=None,
+                                                     noise=None, factor=0.1)
+            X_large, y_large = datasets.make_circles(n_samples=(200, 200), random_state=None,
+                                                     noise=None, factor=0.6)
+            c = np.vstack((X_small,X_large))
+            # c = np.stack((c, y_large))
+            dataset = Bunch(data=c)
+        elif add_dataset == 'blobs 2D':
+            dataset = datasets.make_moons(n_samples=300, noise=0.05)
+            dataset = Bunch(data=dataset[0])
+
+        elif add_dataset == 's_curve 2D':
+            centers = [(3, 3), (0, 0), (5, 5)]
+            dataset = datasets.make_blobs(n_samples=200, centers=centers, shuffle=False, random_state=None)
+            dataset = Bunch(data=dataset[0])
+        else:
+            print('Error to input dataset')
+        if ('2D' in add_dataset):
+            number_of_features = 2
+            add_dimensions = 2
+        else:
+            number_of_features = st.slider('how many features so you want to use?', 0, 30, 2)
+            add_dimensions = st.radio(
+                'Plot with how many dimensions?',
+                (2, 3))
+
+        number_of_clusters = st.slider('number_of_clusters (default 2).', 1, 10, 2)
+        n_epochs = st.slider('n_epochs (default 10)', 0, 100, 10)
+        epsilon_likelihood = st.slider('epsilon_likelihood  (default 1)', 1, 100, 1)
+        # epsilon_likelihood /= 10
+
+    run_GMM = GMM.GMM(dataset, number_of_clusters=number_of_clusters, n_epochs=n_epochs, epsilon_likelihood=epsilon_likelihood)
+    run_GMM.train_gmm()
+    run_GMM.plot_likelihood()
+
+    with col3:
+        with open("GMM.py") as file:
+            btn = st.download_button(
+                label="Download Python Resources File",
+                data=file,
+            )
+
+        if add_dimensions == 3:
+            rotate_fig_0 = st.slider('Rotate axis x', 0, 180, 45, step=45)
+            rotate_fig_1 = st.slider('Rotate axis y', 0, 180, 45, step=45)
+        else:
+            pass
+
+    with col2:
+        if add_dimensions == 2:
+            run_GMM.train_gmm()
+            image = Image.open('plot_likelihood.png')
+            st.image(image)
+            # htp = "https://raw.githubusercontent.com/djswoosh/Music-Recommendation-Engine-using-FMA-Dataset/main/1200px-The_Echo_Nest_logo.svg.png"
+            # st.image(htp, caption='logo', width=350)
+
+        elif add_dimensions == 3:
+            if number_of_features <= 2:
+                st.markdown(f'<h1 style="color:#990000;font-size:22px;">{"cant plot 3D with less then 3 feathers"}</h1>',
+                            unsafe_allow_html=True)
+            else:
+                run_DBSCAN.plot_3d(run_DBSCAN.cluster, rotate_fig_0=rotate_fig_0, rotate_fig_1=rotate_fig_1)
+                image = Image.open('C:\\DBSCAN\\DBSACN_3D.png')
+                st.image(image)
+        else:
+            pass
+
+
+
+
+
+main()
