c70898391a1f6cf0565e3895757a0aac8a24fa33
or_cohen <or20cohen@gmail.com>
2022-06-15 11:31:34 +0300

    new

---
+++ Map.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+
+data = pd.DataFrame({
+    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
+    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
+    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
+})
+
+st.map(data)
