57a998fa28101c292f31917e4e51080c4463003c
or_cohen <or20cohen@gmail.com>
2022-06-20 14:47:26 +0300

    new

---
+++ map2.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import folium
+
+# read excel data as dataframe
+dataDf = pd.read_excel('power_plants.xlsx')
+
+# initialize a map with center and zoom
+mapObj = folium.Map(location=[32.19257001621871, 34.87963762591485],
+                     zoom_start=7, tiles='openstreetmap')
+# folium.TileLayer('stamenterrain', attr="stamenterrain").add_to(mapObj)
+
+# create GeoJson layer to draw country borders
+# style options - https://leafletjs.com/reference-1.7.1.html#path
+bordersStyle = {"color": 'green', 'weight': 2, 'fillOpacity': 0}
+
+# create a layer for bubble map using FeatureGroup
+powerPlantsLayer = folium.FeatureGroup("Power Plants")
+# add the created layer to the map
+powerPlantsLayer.add_to(mapObj)
+
+# iterate through each dataframe row
+for i in range(len(dataDf)):
+    areaStr = dataDf.iloc[i]['area']
+    fuelStr = dataDf.iloc[i]['fuel']
+    capVal = dataDf.iloc[i]['capacity']
+    # derive the circle color
+    clr = "blue" if fuelStr.lower() == 'wind' else "red"
+    # derive the circle radius
+    radius = capVal*100
+    # derive the circle pop up html content
+    popUpStr = 'Area - {0}<br>Fuel - {1}<br>Capacity - {2} MW'.format(
+        areaStr, fuelStr, capVal)
+    # draw a circle for the power plant on the layer
+    folium.Circle(
+        location=[dataDf.iloc[i]['lat'], dataDf.iloc[i]['lng']],
+        popup=folium.Popup(popUpStr, min_width=100, max_width=700),
+        radius=radius,
+        color=clr,
+        weight=2,
+        fill=True,
+        fill_color=clr,
+        fill_opacity=0.1
+    ).add_to(powerPlantsLayer)
+
+
+# add layer control over the map
+folium.LayerControl().add_to(mapObj)
+
+# html to be injected for displaying legend
+legendHtml = '''
+     <div style="position: fixed; 
+     bottom: 50px; left: 50px; width: 150px; height: 70px; 
+     border:2px solid grey; z-index:9999; font-size:14px;
+     ">&nbsp; Fuel Types <br>
+     &nbsp; <i class="fa fa-circle"
+                  style="color:blue"></i> &nbsp; Wind<br>
+     &nbsp; <i class="fa fa-circle"
+                  style="color:red"></i> &nbsp; Solar<br>
+      </div>
+     '''
+
+# inject html corresponding to the legend into the map
+mapObj.get_root().html.add_child(folium.Element(legendHtml))
+
+# save the map as html file
+mapObj.save('output.html')
