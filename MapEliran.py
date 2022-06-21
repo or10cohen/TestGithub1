8ce1389a58be6398d92b05cf1a44a596d82fa9c7
or_cohen <or20cohen@gmail.com>
2022-06-21 12:58:59 +0300

    new

--- MapEliran.py
+++ MapEliran.py
@@ -4,6 +4,22 @@ from streamlit_folium import folium_static
 import folium
 import map2
 
+st.set_page_config(page_title="The Ramsey Highlights", layout="wide")
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
+        width: 400px;
+        margin-left: -400px;
+    }
+
+    """,
+    unsafe_allow_html=True,
+)
+
 st.set_page_config(layout="wide")
 
 location = st.sidebar.selectbox(
