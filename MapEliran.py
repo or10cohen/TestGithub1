a8ca9996bbc6c78cc7dd08b5ed943d36c0ead76c
or_cohen <or20cohen@gmail.com>
2022-06-21 13:14:12 +0300

    new

--- MapEliran.py
+++ MapEliran.py
@@ -7,20 +7,20 @@ import map2
 st.title('Or Yosef Cohen')
 st.header("provisional")
 st.set_page_config(page_title="Or Yosef Cohen", layout="wide")
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-
-    """,
-    unsafe_allow_html=True,
-)
+# st.markdown(
+#     """
+#     <style>
+#     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
+#         width: 400px;
+#     }
+#     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
+#         width: 400px;
+#         margin-left: -400px;
+#     }
+#
+#     """,
+#     unsafe_allow_html=True,
+# )
 
 # st.set_page_config(layout="wide")
 
