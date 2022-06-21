4f4bbd4c6b0504122379ed306eab0a6d64f014be
or_cohen <or20cohen@gmail.com>
2022-06-21 10:36:18 +0300

    new

--- MapEliran.py
+++ MapEliran.py
@@ -4,23 +4,17 @@ from streamlit_folium import folium_static
 import folium
 import map2
 
+st.set_page_config(layout="wide").
 
-Raanana  = pd.DataFrame({
-    'awesome cities' : ['R1', 'R2', 'R3', 'R4'],
-    'lat' : [32.19257001621871, 32.19257001621871, 32.188691433645396, 32.19070709561081],
-    'lon' : [34.87963762591485, 34.86982186463729, 34.86978577727965, 34.86058350108193]
-})
-
-
-Kryot = pd.DataFrame({
-    'awesome cities' : ['H1', 'H2', 'H3', 'H3'],
-    'lat' : [32.83176922748003, 32.83366316684312, 32.8331020038359, 32.834615966742696],
-    'lon' : [35.06681117327111, 35.06722858061457, 35.06481457481161, 35.065990272162324]
-})
-#
 location = st.sidebar.selectbox(
             'Which DataSet do you want to use?',
             ('Kryot', 'Raanana'))
+
+st.sidebar.selectbox(
+    label="What radius do you want to assign?",
+    options=("0.1 mile", "1 mile", "2 miles", "3 miles"),
+    key="radius"
+)
 
 if location == 'Kryot':
     data = Kryot
@@ -29,13 +23,6 @@ elif location == 'Raanana':
 else:
     pass
 
-
-
-st.sidebar.selectbox(
-    label="What radius do you want to assign?",
-    options=("0.1 mile", "1 mile", "2 miles", "3 miles"),
-    key="radius"
-)
 
 folium_static(map2.mapObj)
 
