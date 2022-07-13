56d6172bc5c215a93b5aff217a41b94e45950a17
or_cohen <or20cohen@gmail.com>
2022-07-13 14:25:01 +0300

    new

--- AdaBoost.py
+++ AdaBoost.py
@@ -15,7 +15,7 @@ y = data_set.target
 print(type(X))
 ##-----------------------------------AdaBoost DecisionTreeClassifier----------------------------------------------------
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-X_train_before = X_train.copy
+X_train_before = X_train
 abc = AdaBoostClassifier(base_estimator=None, n_estimators=50, learning_rate=1.0, algorithm='SAMME.R',
                          random_state=None)
 model_abc_dt = abc.fit(X_train, y_train)
@@ -28,11 +28,9 @@ abc = AdaBoostClassifier(base_estimator=svc, n_estimators=50, learning_rate=1.0,
 model_abc_SVC = abc.fit(X_train, y_train)
 y_pred_abc_SVC = model_abc_SVC.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred_abc_SVC))
-X_train = np.array(X_train)
-print(type(X_train))
-print('X_train_before', X_train_before)
-print('X_train', X_train)
-# print(np.subtract((X_train, X_train_before)))
+
+
+print(X_train - X_train_before)
 # print(X_train - X_train_before)
 
 
