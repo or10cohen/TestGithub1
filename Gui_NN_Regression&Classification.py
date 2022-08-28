bd7b7de20669da11db1d9f2535333dbba9d61ea4
or_cohen <or20cohen@gmail.com>
2022-08-28 14:53:12 +0300

    new

--- Gui_NN_Regression&Classification.py
+++ Gui_NN_Regression&Classification.py
@@ -45,10 +45,10 @@ def main():
         No_hidden_layers = st.number_input('No_hidden_layer', value=3)
         st.write('No_hidden_layers:', No_hidden_layers)
         No_neurons_per_layer = st.text_input('No_neurons_per_layers = [int list (default=4)]: \n '
-                                             'example for 3 hidden layer: [4, 4, 4]', value='None')
+                                             'example for 3 layer: [4, 4, 4]', value='None')
         st.write('No_neurons_per_layer:', No_neurons_per_layer)
         activation_per_layer = st.text_input('activation_per_layer = [string list (default=string(relu))]: \n '
-                                             'example for 3 hidden layer: [\'relu\',\'relu\', \'relu\']', value='None')
+                                             'example for 3 layer: [\'relu\',\'relu\', \'relu\']', value='None')
         st.write('No_neurons_per_layer:', activation_per_layer)
 
 
