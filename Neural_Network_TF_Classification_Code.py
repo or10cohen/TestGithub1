ece44968cbd0a359832050354b79f1a66f40caa3
or_cohen <or20cohen@gmail.com>
2022-08-28 14:29:07 +0300

    new

--- Neural_Network_TF_Classification_Code.py
+++ Neural_Network_TF_Classification_Code.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from PIL import Image
-#import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
