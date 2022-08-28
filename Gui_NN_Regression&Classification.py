50ccf624d83228d91a4537161b971bdbba704980
or_cohen <or20cohen@gmail.com>
2022-08-28 14:16:17 +0300

    new

---
+++ Gui_NN_Regression&Classification.py
@@ -0,0 +1,147 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import Neural_Network_TF_Regression_Code
+import Neural_Network_TF_Classification_Code
+from PIL import Image
+# from dash import html
+# import dash_bootstrap_components as dbc
+
+
+def main():
+    epsilon = 0.000001
+    st.title('Varona - by Oren Niazov')
+    with st.sidebar:
+
+        Datatype = st.selectbox(
+            'choose Data type',
+            ('Regression', 'Classification'))
+        st.write('Datatype:', Datatype)
+
+        if Datatype=='Regression':
+            Dataset = st.selectbox(
+                'choose Dataset',
+                ('fake_regression0', 'fake_regression1', 'fake_regression2'))
+            st.write('Dataset:', Dataset)
+
+        elif Datatype=='Classification':
+            Dataset = st.selectbox(
+                'choose Dataset',
+                ('fake_classification0', 'classification1', 'fake_classification2'))
+            st.write('Dataset:', Dataset)
+        else:
+            print('pay attention: something wrong with the Datatype choose')
+
+
+        st.title('parameters for: split_and_normalize_data def')
+        test_size = st.number_input('test_size', value=0.3, format="%.10f")
+        st.write('test_size:', test_size)
+        random_state = st.number_input('random_state:', value=42, format="%.10f")
+        st.write('random_state:', random_state)
+
+
+        st.title('parameters for: create_neural_network def')
+        No_hidden_layers = st.number_input('No_hidden_layer', value=3)
+        st.write('No_hidden_layers:', No_hidden_layers)
+        No_neurons_per_layer = st.text_input('No_neurons_per_layers = [int list (default=4)]:', value='None')
+        st.write('No_neurons_per_layer:', No_neurons_per_layer)
+        activation_per_layer = st.text_input('activation_per_layer = [string list (default=string(relu))]:', value='None')
+        st.write('No_neurons_per_layer:', activation_per_layer)
+
+
+        st.title('parameters for: run_model def')
+        optimizer = st.selectbox(
+            'choose optimizer',
+            ('rmsprop', 'rmsprop', 'rmsprop'))
+        st.write('optimizer', optimizer)
+        loss = st.selectbox(
+            'choose loss function',
+            ('mse', 'mse', 'mse'))
+        st.write('loss', loss)
+
+
+        if Dataset=='fake_regression0':
+            df = pd.read_csv('DATA/fake_reg.csv')
+            X = df[['feature1', 'feature2']].values
+            y = df['price'].values
+        elif Dataset=='fake_regression1':
+            Dataset = None
+        elif Dataset=='fake_regression2':
+            Dataset = None
+        elif Dataset=='fake_classification0':
+            Dataset = None
+        elif Dataset == 'fake_classification1':
+            Dataset = None
+        elif Dataset == 'fake_classification2':
+            Dataset = None
+        else:
+            print('pay attention: something wrong with the Dataset choose')
+
+
+        ##----------------------download files you want to share-----------------------
+        with open("CalcVrect.py") as file:
+            btn = st.download_button(
+                label="Download CalcVrect Python Resources File",
+                data=file,
+            )
+        with open("Varona_k_range.py") as file:
+            btn = st.download_button(
+                label="Download Varona_k_range Python Resources File",
+                data=file,
+            )
+        with open("Gui_Varona.py") as file:
+            btn = st.download_button(
+                label="Download Gui_Varona Python Resources File",
+                data=file,
+            )
+
+    run = Neural_Network_TF_Regression_Code.FirsRegressionNeuralNetwork(X, y)
+    run.split_and_normalize_data()
+    run.create_neural_network()
+    run.run_model()
+    run.epochs_graph()
+    run.predict()
+    run.save_and_load_model()
+
+##-----------------------------------------------------------------------------------------------------------
+    # Grpah = st.radio(
+    #     "Choose graph",
+    #     ('Tx and Rx RMS current Load', 'Tx and Rx RMS current Min Load', 'arona - Efficency'))
+    #
+    # if Grpah == 'Tx and Rx RMS current Load':
+    #     st.image(Varona0, caption='Sunrise by the mountains')
+    # elif Grpah == 'Tx and Rx RMS current Min Load':
+    #     st.image(Varona1, caption='Sunrise by the mountains')
+    # elif Grpah == 'arona - Efficency':
+    #     st.image(Varona2, caption='Sunrise by the mountains')
+    # else:
+    #     st.write("You didn't select comedy.")
+
+##-----------------------------------------------------------------------------------------------------------
+    # carousel = dbc.Carousel(
+    #     items=[
+    #         {"key": "1", "src": "Varona - Vrect loaded (200W) vs. Min load - FB DC = 20 present.png"},
+    #         {"key": "2", "src": "Varona - Efficency.png"},
+    #         {"key": "3", "src": "Varona - Tx and Rx RMS current Min Load.png"},
+    #         {"key": "3", "src": "Varona - Tx and Rx RMS current Min Load.png"},
+    #     ],
+    #     controls=True,
+    #     indicators=True,
+    # )
+##-----------------------------------------------------------------------------------------------------------
+main()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
---
+++ Neural_Network_TF_Classification_Code.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+
+class FirsNeuralNetwork:
+    def __init__(self, X, y, n_epochs=250):
+        self.X = X
+        self.y = y
+        self.n_epochs = n_epochs
+
+    def split_and_normalize_data(self):
+        X_train, X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.3, random_state=42)
+        # X_train, X_test = MinMaxScaler().fit_transform(X_train), MinMaxScaler().fit_transform(X_test)
+        self.scalar = MinMaxScaler()
+        self.scalar.fit(X_train)
+        self.scalar.fit(X_test)  ##????
+        self.X_train, self.X_test = self.scalar.transform(X_train), self.scalar.transform(X_test)
+
+    def create_neural_network(self):
+       pass
+
+    def run_model(self, optimizer='rmsprop', loss='mse'):   #loss classification = softmax(נותן לכל נוירון באווטפוט יחס לתשובה הנכונה, הסכום שווה ל1)
+       pass
+
+    def epochs_graph(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def accuracy(self):
+        pass
+
+    def save_and_load_model(self):
+        ###-----------------------save and load your model----------------------------------------
+        self.model2.save('C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\')
+        loaded_model = tf.keras.models.load_model('C:\\Users\\or_cohen\\PycharmProjects\\TestGithub1\\')
+        loaded_model.predict(self.new_gem)
+        print(Fore.MAGENTA + 'new model predictions on new data without label', loaded_model.predict(self.new_gem))
+        if loaded_model.predict(self.new_gem) == self.model2.predict(self.new_gem):
+            print(Fore.GREEN + 'the loaded_model = save model. good save and load!')
+        else:
+            print(Fore.GREEN + 'error load or save model!')
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('DATA/fake_reg.csv')
+    X = df[['feature1', 'feature2']].values
+    y = df['price'].values
+    run = FirsNeuralNetwork(X, y)
+    run.split_and_normalize_data()
+    run.create_neural_network()
+    run.run_model()
+    run.epochs_graph()
+    run.predict()
+    run.save_and_load_model()
