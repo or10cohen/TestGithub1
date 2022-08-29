5af877504c21db3fa49420ae1b55386d55f1ff89
or_cohen <or20cohen@gmail.com>
2022-08-29 17:31:44 +0300

    new

--- Gui_NN_Regression&Classification.py
+++ Gui_NN_Regression&Classification.py
@@ -24,16 +24,20 @@ def main():
         st.title('parameters for function: create_neural_network')
         No_hidden_layers = st.number_input('No_hidden_layer', value=3)
         st.write('No_hidden_layers:', No_hidden_layers)
+
+
         No_neurons_per_layer = st.text_input('No_neurons_per_layers = [int list (default=4)]: \n '
-                                             'example&default 3 layer: 444 \n', value='None')
-        if No_neurons_per_layer == 'None':
+                                             'example&default 3 layer: 444 \n', value='Nothing')
+        if No_neurons_per_layer == 'Nothing':
             No_neurons_per_layer = None
+        else:
+            No_neurons_per_layer = [int(i) for i in str(No_neurons_per_layer)]
         st.write('No_neurons_per_layer:', No_neurons_per_layer)
+
         activation_per_layer = st.text_input('activation_per_layer = [string list (default=\'relu\')]: \n '
                                              'example&default 3 layer: relu relu relu'
-                                             'but you can use: sigmoid, tanh and more', value='None')
-
-        if activation_per_layer == 'None':
+                                             'but you can use: sigmoid, tanh and more', value='Nothing')
+        if activation_per_layer == 'Nothing':
             activation_neurons_per_layer = None
         else:
             def Convert(string):
@@ -43,12 +47,7 @@ def main():
         st.write('No_neurons_per_layer:', activation_per_layer)
 
 
-        if No_neurons_per_layer == 'None':
-            No_neurons_per_layer = None
-        else:
-            No_neurons_per_layer = [int(i) for i in str(No_neurons_per_layer)]
 
-        st.write('No_neurons_per_layer:', No_neurons_per_layer)
 
 
 
