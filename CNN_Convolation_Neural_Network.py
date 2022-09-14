576ce22934e2cfbc669c3c7443b2c70b9b203317
or_cohen <or20cohen@gmail.com>
2022-09-14 14:48:18 +0300

    new

--- CNN_Convolation_Neural_Network.py
+++ CNN_Convolation_Neural_Network.py
@@ -18,7 +18,7 @@ class CNN:
 
     def split_and_normalize_data(self, data, DataType='Mnist'):
         (self.X_train, self.y_train), (self.X_test, self.y_test) = data
-        self.X_train, self.y_train = self.X_train / 255, self.y_train / 255 #normalize_data
+        self.X_train, self.X_test = self.X_train / 255.0, self.X_test/ 255.0 #normalize_data
         print('X_train.shape:\n', self.X_train.shape, '\ny_train.shape:\n', self.y_train.shape, '\nX_test.shape:\n', self.X_test.shape, '\ny_test.shape:\n', self.y_test.shape)
         if DataType=='Mnist':
             # self.X_train = tf.reshape(self.X_train, [self.X_train.shape[0], 28, 28, 1]) #from N * 28 * 28 to N * 28 * 28 * 1
@@ -29,6 +29,8 @@ class CNN:
             self.y_train = self.y_train.flatten()
             #self.y_test = tf.keras.layers.Flatten()(self.y_test)
             self.y_test = self.y_test.flatten()
+            print('max(self.y_train)', max(self.y_train))
+            print('max(self.y_test)', max(self.y_test))
         elif DataType == 'CIFAR':
             pass
         else:
@@ -48,10 +50,10 @@ class CNN:
         x = tf.keras.layers.Dense(512, activation='relu')(x)
         print('len(np.unique(self.y_train)):', len(np.unique(self.y_train)))
         x = tf.keras.layers.Dropout(0.2)(x)
-        x = tf.keras.layers.Dense(len(np.unique(self.y_train[0])), activation='softmax')(x)
-        self.model = tf.keras.Model(i, x)
+        x = tf.keras.layers.Dense(len(np.unique(cnn.y_train)), activation='softmax')(x)
+        self.model = tf.keras.models.Model(i, x)
 
-    def run_model(self, optimizer='adam', loss='BinaryCrossentropy', batch_size=32, n_epochs=15):
+    def run_model(self, optimizer='adam', loss='sparse_categorical_crossentropy', batch_size=32, n_epochs=50):
         self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
         self.model.fit(x=self.X_train, y=self.y_train, validation_data=(self.X_test, self.y_test), epochs=n_epochs, batch_size=batch_size)
 
